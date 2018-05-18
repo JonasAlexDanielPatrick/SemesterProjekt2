9cf100752ad27ba223e08bcf20d6456730b5f047
Jonas Bill Lundqvist <jona124j@edu.eal.dk>
2018-05-18 12:24:27 +0200

    Ur update

--- SemesterProjekt2/Main/MainWindow.xaml.cs
+++ SemesterProjekt2/Main/MainWindow.xaml.cs
@@ -1,36 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Threading;
+
 
 namespace Main
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        public static MainWindow instance;
+
         public MainWindow()
         {
             InitializeComponent();
 
+            instance = this;
+
             Connection.Connect();
 
+            Thread threadLondonUr = new Thread(new ThreadStart(Ur.London));
+            Thread threadKøbenhavnUr = new Thread(new ThreadStart(Ur.København));
+
+            threadLondonUr.Start();
+            threadKøbenhavnUr.Start();
         }
 
-        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        internal string LondonUr
         {
-
+            get { return LabelLondon.Content.ToString(); }
+            set { Dispatcher.Invoke(new Action(() => { LabelLondon.Content = value; })); }
         }
+
+        internal string KøbenhavnUr
+        {
+            get { return LabelKøbenhavn.Content.ToString(); }
+            set { Dispatcher.Invoke(new Action(() => { LabelKøbenhavn.Content = value; })); }
+        }
+
+
     }
 }
---
+++ SemesterProjekt2/Main/Ur.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Main
+{
+    class Ur
+    {
+
+        public static string london = "";
+        public static string københavn = "";
+
+        public static void London()
+        {
+            while (true)
+            {
+                string hour = DateTime.Now.Hour.ToString("00");
+                string minute = DateTime.Now.Minute.ToString("00");
+
+                london = String.Format("London: {0}:{1}", hour, minute);
+
+                MainWindow.instance.LondonUr = london;
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void København()
+        {
+            while (true)
+            {
+                string hour = DateTime.Now.Hour.ToString("00");
+                string minute = DateTime.Now.Minute.ToString("00");
+
+                københavn = String.Format("København: {0}:{1}", hour, minute);
+
+                MainWindow.instance.KøbenhavnUr = københavn;
+
+                Thread.Sleep(1000);
+            }
+        }
+
+    }
+}
