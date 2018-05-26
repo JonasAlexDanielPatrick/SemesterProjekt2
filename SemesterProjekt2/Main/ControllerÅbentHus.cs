de45a174c634ca9ca0a5f972c742caca94d9c62e
Thondar <aj32142063>
2018-05-26 20:43:38 +0200

    ControllerÅbentHus og ModelÅbentHus er oprettet

---
+++ SemesterProjekt2/Main/ControllerÅbentHus.cs
@@ -0,0 +1,41 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Diagnostics;
+
+namespace Main
+{
+    class ControllerÅbentHus
+    {
+
+        public static void GenererListe(string udfil)
+        {
+
+
+            Udskriv(udfil);
+        }
+
+        public static void Udskriv(string udfil)
+        {
+            StreamWriter stream = null;
+
+            try
+            {
+                // Udskriv fil her!
+
+            }
+            catch (System.Exception)
+            {
+
+            }
+            finally
+            {
+                if (stream != null)
+                {
+                    stream.Close();
+                }
+            }
+        }
+
+    }
+}
---
+++ SemesterProjekt2/Main/ModelÅbentHus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class ModelÅbentHus
+    {
+        public string Mægler { get; }
+        public int Sagsnr { get; }
+        public string Adresse { get; }
+        public string Område { get; }
+        public string By { get; }
+        public int Pris { get; }
+        
+
+        public ModelÅbentHus(string mægler, int sagsnr, string adresse, string område, string by, int pris)
+        {
+            Mægler = mægler;
+            Sagsnr = sagsnr;
+            Adresse = adresse;
+            Område = område;
+            By = by;
+            Pris = pris;
+        }
+    }
+}
